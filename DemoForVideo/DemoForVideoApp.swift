9fa508c4c94394581578173b29194f50652d9124
Pac12Run8000 <gangstarr78992@>
2023-08-29 06:14:08 -0700

    Initial Commit

---
+++ DemoForVideo/DemoForVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoForVideoApp.swift
+//  DemoForVideo
+//
+//  Created by Michelle Grover on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoForVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
